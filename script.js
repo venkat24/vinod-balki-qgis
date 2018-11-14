8e7ae78d18935929240169dc9c1dcfd6cd5d6a87
Gautham Kumar <heygautham@gmail.com>
2018-11-14 15:17:47 +0530

    Completed map design
    - Added grandma house
    - Added forest
    - Added path for red riding hood to walk
    - Added collision detection

--- script.js
+++ script.js
@@ -1,16 +1,85 @@
 const constants = {
-    houseSpriteLocation: {
-        x: 20,
-        y: 30
+    momHouseSprite: {
+        x: 0,
+        y: 40,
+        size: 13
     },
-    forestSpriteLocation: {
-        x: 300,
-        y: 300
+    grandmaHouseSprite: {
+        x: 450,
+        y: 40,
+        size: 13
     },
-    redRidingHoodStartLocation: {
-        x: 130,
-        y: 100
+    forestSprite: {
+        x: 295,
+        y: 350,
+        size: 4
+    },
+    redRidingHood: {
+        startLocation: {
+            x: 130,
+            y: 130
+        },
+        spriteSize: {
+            width: 32,
+            height: 48
+        },
+        actualSize: {
+            width: 32,
+            height: 48
+        }
+    },
+    pathWidth: 70,
+}
+
+class PathBlock {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
+
+    draw(ctx) {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    checkIfInsidePathBlock(x, y) {
+        let isInsideX = false;
+        let isInsideY = false;
+        if (this.height < 0) {
+            isInsideY = (y<=this.y && y>=(this.y+this.height));
+        } else {
+            isInsideY = (y>=this.y && y<=(this.y+this.height));
+        }
+
+        if (this.width < 0) {
+            isInsideX = (x<=this.x && x>=(this.x+this.width));
+        } else {
+            isInsideX = (x>=this.x && x<=(this.x+this.width));
+        }
+
+        if (isInsideX && isInsideY) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+let path = [
+    new PathBlock(120,120,310,constants.pathWidth),
+    new PathBlock(220,120,constants.pathWidth, 300),
+    new PathBlock(220,420,480,constants.pathWidth),
+    new PathBlock(700,420+constants.pathWidth,constants.pathWidth, -300),
+    new PathBlock(700+constants.pathWidth,120,-200,constants.pathWidth)
+]
+
+let isInsideAPath = (x, y) => {
+    isInsidePath = false;
+    for (var i = 0; i < path.length; i++) {
+        isInsidePath = isInsidePath || path[i].checkIfInsidePathBlock(x,y);
+    }
+    return isInsidePath;
 }
 
 class RedRidingHood {
@@ -41,18 +110,34 @@ class RedRidingHood {
     update() {
         if (this.movingDirection.right) {
             this.x += this.speed;
+            if (!isInsideAPath(this.x+constants.redRidingHood.actualSize.width/2, this.y+constants.redRidingHood.actualSize.height)) {
+                this.x -= this.speed;
+            }
+
             this.state = 2;
             this.animationStep = (this.animationStep + this.animationSpeed) % 4;
         } else if (this.movingDirection.left) {
             this.x -= this.speed;
+            if (!isInsideAPath(this.x+constants.redRidingHood.actualSize.width/2, this.y+constants.redRidingHood.actualSize.height)) {
+                this.x += this.speed;
+            }
+
             this.state = 1;
             this.animationStep = (this.animationStep + this.animationSpeed) % 4;
         } else if (this.movingDirection.up) {
             this.y -= this.speed;
+            if (!isInsideAPath(this.x+constants.redRidingHood.actualSize.width/2, this.y+constants.redRidingHood.actualSize.height)) {
+                this.y += this.speed;
+            }
+
             this.state = 3;
             this.animationStep = (this.animationStep + this.animationSpeed) % 4;
         } else if (this.movingDirection.down) {
             this.y += this.speed;
+            if (!isInsideAPath(this.x+constants.redRidingHood.actualSize.width/2, this.y+constants.redRidingHood.actualSize.height)) {
+                this.y -= this.speed;
+            }
+
             this.state = 0;
             this.animationStep = (this.animationStep + this.animationSpeed) % 4;
         }
@@ -80,6 +165,13 @@ let imageLoader = (imagePath) => {
     })
 }
 
+let drawPath = (ctx) => {
+    ctx.fillStyle="#afafaf";
+    for (var i = 0; i < path.length; i++) {
+        path[i].draw(ctx);
+    }
+}
+
 $(document).ready(async () => {
 
     let animate = await (async() => {
@@ -89,12 +181,12 @@ $(document).ready(async () => {
         // Load assets
         let bgImage = await imageLoader('images/bg.jpg');
         let houseSprite = await imageLoader("sprites/houseSprite.png");
-        let forestSprite = await imageLoader("sprites/forestSprite.png");
         let redRidingHoodSprite = await imageLoader("sprites/redRidingHoodSprite.png");
+        let forestSprite = await imageLoader("sprites/forestSprite.png");
 
         let redRidingHood = new RedRidingHood(
-            constants.redRidingHoodStartLocation.x,
-            constants.redRidingHoodStartLocation.y,
+            constants.redRidingHood.startLocation.x,
+            constants.redRidingHood.startLocation.y,
             redRidingHoodSprite
         );
 
@@ -126,33 +218,40 @@ $(document).ready(async () => {
             // Clear the canvas
             ctx.clearRect(0, 0, 800, 600);
             ctx.drawImage(bgImage, 0, 0, 800, 600);
+            drawPath(ctx);
             ctx.drawImage(
                 houseSprite,
-                constants.houseSpriteLocation.x,
-                constants.houseSpriteLocation.y,
-                140,
-                110
+                constants.momHouseSprite.x,
+                constants.momHouseSprite.y,
+                14*constants.momHouseSprite.size,
+                11*constants.momHouseSprite.size
             );
-            redRidingHood.update();
+            ctx.drawImage(
+                houseSprite,
+                constants.grandmaHouseSprite.x,
+                constants.grandmaHouseSprite.y,
+                14*constants.grandmaHouseSprite.size,
+                11*constants.grandmaHouseSprite.size
+            );
 
+            redRidingHood.update();
             ctx.drawImage(redRidingHoodSprite,
-                Math.floor(redRidingHood.animationStep) * 32, // sprite offset
-                redRidingHood.state * 48,
-                32, // sprite width
-                48, // sprite height
+                Math.floor(redRidingHood.animationStep) * constants.redRidingHood.spriteSize.width, // sprite offset
+                redRidingHood.state * constants.redRidingHood.spriteSize.height,
+                constants.redRidingHood.spriteSize.width,
+                constants.redRidingHood.spriteSize.height,
                 redRidingHood.x,
                 redRidingHood.y,
-                32, // actual width
-                48 // actual height
+                constants.redRidingHood.actualSize.width,
+                constants.redRidingHood.actualSize.height
             );
 
-            // The forest draws on top of red riding hood
             ctx.drawImage(
                 forestSprite,
-                constants.forestSpriteLocation.x,
-                constants.forestSpriteLocation.y,
-                230,
-                190
+                constants.forestSprite.x,
+                constants.forestSprite.y,
+                69*constants.forestSprite.size,
+                45*constants.forestSprite.size
             );
 
             requestAnimationFrame(animate);
